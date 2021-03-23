950bfbf30aca2ad4111be65487843c2fb4bd581d
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-03-23 23:30:21 +0500

    html halper done lossly type

---
+++ WebApplication16/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication16.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult about()
+        {
+            ViewBag.a = "tosiq ahmed";
+            ViewData["b"] = "laiq ahmed";
+            TempData["c"] = 12 + 78;
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
